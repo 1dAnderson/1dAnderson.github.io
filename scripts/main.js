729277c13cb36beb9fdc08c1efe0994eef44b72e
1dAnderson <64841244+1dAnderson@users.noreply.github.com>
2025-02-14 20:47:22 +0800

    新文档

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,8 @@
-//图片替换
-/*let myImage = document.querySelector('img');
-myImage.onclick = function(){
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'image/t2.jpeg'){
-        myImage.setAttribute('src','image/tt.jpeg');
-    }else{
-        myImage.setAttribute('src','image/t2.jpeg')
-    }
-}
-//点击提示
-document.querySelector('html').onclick = function(){
-    alert("don't click me")
-}*/
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName(){
-    let myName = prompt('请输入你的名字');
-    if(myName === null){
-        localStorage.setItem('name',myName);
-        myHeading.textContent = '好好学习噢,' + myName;
-    }
-    else if(!myName){
-        setUserName();
-    }
-    else{
-        localStorage.setItem('name',myName);
-        myHeading.textContent = '好好学习噢,' + myName;
-    }
-}
-
-if(!localStorage.getItem('name')){
-    setUserName();
-}
-else{
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = '好好学习，' + storedName;
-}
-
-myButton.onclick = function(){
-    setUserName();
-}
-
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
